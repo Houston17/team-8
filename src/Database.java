6aa8d3115dca6ed383553ab699fe26b6daf8ff2b
Acciaccatura <2899523@gmail.com>
2017-10-14 01:11:06 -0500

    postgres test

--- src/Database.java
+++ src/Database.java
@@ -1,12 +1,13 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database {
 
-	private static final String sqlURL = "jdbc:mysql://172.31.62.139:3306";
-	private static final String sqlUser = "root";
-	private static final String sqlPass = "root";
+	private static final String sqlURL = "jdbc:postgres://ec2-23-21-189-181.compute-1.amazonaws.com:5432/dfiapi0mdogp1g";
+	private static final String sqlUser = "mvdwiysxpajrkk";
+	private static final String sqlPass = "b4c48e991b2834cd698e238618abd4a74dce314b14cf43a1cb80e309dcb3ae84";
 	
 	private final Connection sql;
 	
@@ -15,6 +16,9 @@ public class Database {
 		try {
 			temp = DriverManager.getConnection(sqlURL, sqlUser, sqlPass);
 			System.out.println("Connected to database");
+			java.sql.Statement s = temp.createStatement();
+			ResultSet rs = s.executeQuery("SELECT name_test FROM USERS");
+			System.out.println(rs.getInt("name_test"));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
