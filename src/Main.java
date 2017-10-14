9dcea9498c59b67f110cc8aa89a9af6862344274
Acciaccatura <2899523@gmail.com>
2017-10-13 23:17:51 -0500

    dev stuff y'know

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		get("/", (req, res) -> "hey nerds");
 		System.out.println("Running on port " + Spark.port());
+		Database data = new Database();
 	}
 }
