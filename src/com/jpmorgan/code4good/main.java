c8ad22e0e0c948418782d72b84ac3a65c3df2e63
Acciaccatura <2899523@gmail.com>
2017-10-13 20:06:08 -0500

    first commit

---
+++ src/com/jpmorgan/code4good/main.java
@@ -0,0 +1,11 @@
+package com.jpmorgan.code4good;
+
+import static spark.Spark.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		port(666);
+		get("/", (req, res) -> "hey nerds");
+	}
+}
